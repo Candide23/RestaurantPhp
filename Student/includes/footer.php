4daf9120eb20995128a0a1161b13fec9749711f6
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2023-06-21 19:31:29 -0500

    first commit

---
+++ Student/includes/footer.php
@@ -0,0 +1,41 @@
+
+<div id="footer" class="cf">
+
+    <div class="column three">
+        <strong>Phone</strong>
+        803.555.3810
+    </div>
+
+    <div class="column three">
+        <strong>Location</strong>
+        123 Lincoln Av
+        Chicago, IL
+
+    </div>
+
+    <div class="column three last">
+        <strong>Hours</strong>
+        <em>Tuesday - Thursday</em><br>
+        1:00pm - 9:00pm<br><br>
+
+        <em>Friday - Saturday</em><br>
+        4:00pm - 11:00pm<br><br>
+
+        <em>Sunday - Saturday</em><br>
+        close<br><br>
+
+    </div>
+
+</div>
+
+<small>&copy;20023 Candide Mbk</small>
+
+</div>
+
+
+</div>
+
+</div>
+
+</body>
+</html>
---
+++ Student/includes/header.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <title></title>
+    <link href="/assets/styles.css" rel="stylesheet" >
+
+</head>
+<body id="final-example">
+
+<div class="wrapper">
+
+    <div id="banner">
+        <a href="/" title="Return to Home">
+            <img src="img/banner.png" alt="Franklin's Fine Dining">
+
+        </a>
+
+    </div>
+
+    <div id="nav">
+      <?php include('includes/nav.php'); ?>
+    </div>
+
+    <div class="content">
