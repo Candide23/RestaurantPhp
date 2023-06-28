0f126f5c2d5c1bd90d42bba1d785e32c5f0591d8
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2023-06-28 10:25:40 -0500

    Form Validation

--- Student/index.php
+++ Student/index.php
@@ -8,12 +8,10 @@ include('includes/header.php');
     <hr>
 
     <h1>Franklin's Philosophy of Fine Dining</h1>
-    <p>Here at Franklin's, we know that good food isn't just about how expensive the dish is. We're not
-        pompous, we're proud. We're proud of our work, our quality, our environment, and our love for food and family.</p>
-
+    <p>Here at Franklin's, we know that good food isn't just about how expensive the dish is. We're not pompous, we're proud. We're proud of our work, our quality, our environment, and our love for food and family.</p>
     <p>Oh, and you can call us Frankie's. You're family here.</p>
-    <hr>
 
-<?php
-include('includes/footer.php')
-?>
+    <hr>
+</div><!-- philosophy -->
+
+<?php include('includes/footer.php'); ?>
